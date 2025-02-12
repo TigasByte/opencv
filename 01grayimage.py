088c9381602f90e284b037dafebb3691301497e8
Tiago Batista <tiagobatistaa3@gmail.com>
2025-02-12 13:40:48 -0300

    simple program to convert image to gray

--- 01grayimage.py
+++ 01grayimage.py
@@ -7,3 +7,7 @@ else:
     cv2.imshow('Minha imagem',n)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+    imagem_cinza = cv2.cvtColor(n,cv2.COLOR_BGR2GRAY)
+    cv2.imshow('Imagem cinza',imagem_cinza)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
